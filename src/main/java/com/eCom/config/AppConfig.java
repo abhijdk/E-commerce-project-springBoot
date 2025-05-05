b0968c2e34c90617182129fb1cecf34f38b362b6
ABHINNA SUNDAR BEHURIA <abhinnabehuria@outlook.com>
2025-05-05 23:44:08 +0530

    Ony add setup

--- src/main/java/com/eCom/config/AppConfig.java
+++ src/main/java/com/eCom/config/AppConfig.java
@@ -1,17 +1,17 @@
 package com.eCom.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Arrays;
 
 @Configuration
 public class AppConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
-        httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().
-                authorizeHttpRequests(Authorize->requestM)
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         return null;
     }
 }
